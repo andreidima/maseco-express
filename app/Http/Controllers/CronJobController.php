aaf22114a79de981d75162fad1d278f984d63593
‘Andrei <andrei.dima@usm.ro>
2023-03-07 15:45:27 +0200

    a

--- app/Http/Controllers/CronJobController.php
+++ app/Http/Controllers/CronJobController.php
@@ -21,6 +21,7 @@ class CronJobController extends Controller
 
         if ($key === $config_key){
 
+            // Trimitere mesaje catre transportatori
             $comenzi = Comanda::with('locuriOperareIncarcari', 'locuriOperareDescarcari')
                 ->whereHas('locuriOperareIncarcari', function($query){
                     $query->where('data_ora', '<=', Carbon::now()->todatetimestring());
@@ -52,7 +53,7 @@ class CronJobController extends Controller
                 //     foreach ($comanda->locuriOperareDescarcari as $locOperareDescarcare){
                 //         echo $locOperareDescarcare->pivot->ordine . '. ' . $locOperareDescarcare->pivot->data_ora . ', ';
                 //     }
-                //     echo '<br><br>';
+                //     echo '<br><br><br><br>';
                 // }
                 // dd('stop');
 
@@ -66,6 +67,45 @@ class CronJobController extends Controller
                 if (isset($comanda->transportator->email)){
                     Mail::to($comanda->transportator->email)->send(new \App\Mail\ComandaStatus($comanda));
                 }
+            }
+
+
+            // Trimitere mesaj catre Maseco, doar o singura data, in momentul in care incepe prima incarcare din comanda
+            $comenzi = Comanda::with('locuriOperareIncarcari', 'locuriOperareDescarcari')
+                ->whereHas('locuriOperareIncarcari', function($query){
+                    $query->where('ordine', 1)
+                        ->where('data_ora', '<=', Carbon::now()->addMinutes(4)->todatetimestring())
+                        ->where('data_ora', '>=', Carbon::now()->subMinutes(4)->todatetimestring());
+                })
+                ->get();
+
+                // Afisare in pagina pentru debug
+                foreach ($comenzi as $comanda){
+                    // echo 'Comanda id: ' . $comanda->id;
+                    // echo '<br>';
+                    // if (isset($comanda->transportator->email)){
+                    //     echo 'Email transportator: ' . $comanda->transportator->email . '<br>';
+                    // }
+                    // echo '<br>';
+                    // echo 'Incarcari: ';
+                    // foreach ($comanda->locuriOperareIncarcari as $locOperareIncarcare){
+                    //     echo $locOperareIncarcare->pivot->ordine . '. ' . $locOperareIncarcare->pivot->data_ora . ', ';
+                    // }
+
+                    // echo '<br>';
+
+                    // echo 'Descarcari: ';
+                    // foreach ($comanda->locuriOperareDescarcari as $locOperareDescarcare){
+                    //     echo $locOperareDescarcare->pivot->ordine . '. ' . $locOperareDescarcare->pivot->data_ora . ', ';
+                    // }
+                    // echo '<br><br>';
+                }
+                // dd('stop');
+
+
+            foreach ($comenzi as $comanda){
+                // Trimitere email
+                Mail::to('andrei.dima@usm.ro')->send(new \App\Mail\ComandaIncepere($comanda));
             }
 
 
