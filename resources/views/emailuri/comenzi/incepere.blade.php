6a7d3f038a8197a019f2d1180e3ab7146b9c872b
‘Andrei <andrei.dima@usm.ro>
2023-03-13 09:29:16 +0200

    a

--- resources/views/emailuri/comenzi/incepere.blade.php
+++ resources/views/emailuri/comenzi/incepere.blade.php
@@ -3,12 +3,17 @@
 
         @include ('emailuri.headerFooter.header')
 
+        @php
+            $data_ora_prima_incarcare = $comanda->locuriOperareIncarcari()->where('ordine', 1)->data_ora;
+        @endphp
+
+
         <div style="padding:20px 20px; max-width:760px!important;margin:0 auto; font-size:18px">
             Bună Maseco Expres,
             <br><br>
             Comanda {{ $comanda->transportator_contract }} începe prima încărcare
-            la ora <b>{{ $comanda->data_ora ?? \Carbon\Carbon::parse($comanda->data_ora)->isoFormat('HH:mm') }}</b>,
-            în data de <b>{{ $comanda->data_ora ?? \Carbon\Carbon::parse($comanda->data_ora)->isoFormat('DD.MM.YYYY') }}</b>
+            la ora <b>{{ $data_ora_prima_incarcare ? \Carbon\Carbon::parse($data_ora_prima_incarcare)->isoFormat('HH:mm') : '' }}</b>,
+            în data de <b>{{ $data_ora_prima_incarcare ? \Carbon\Carbon::parse($data_ora_prima_incarcare)->isoFormat('DD.MM.YYYY'): '' }}</b>
             <br><br>
             Acesta este un mesaj automat. Vă rugăm să nu răspundeți la acest e-mail.
             <br><br>
