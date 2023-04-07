0df66173c9f4b2fde070f4dd83fcb5b315ea8f65
‘Andrei <andrei.dima@usm.ro>
2023-04-07 08:59:38 +0300

    a

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        {{-- <li class="nav-item me-3 dropdown">
+                        <li class="nav-item me-3 dropdown">
                             <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" title="Fisiere">
                                 <i class="fa-solid fa-folder"></i>
                             </a>
@@ -61,7 +61,7 @@
                                     </a>
                                 </li>
                             </ul>
-                        </li> --}}
+                        </li>
                         <li class="nav-item me-3">
                             <a class="nav-link active" aria-current="page" href="/file-manager" title="File Manager">
                                 <i class="fa-solid fa-folder"></i>&nbsp;
