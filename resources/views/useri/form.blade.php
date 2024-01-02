fd3dedee3a64c5576337f4e7afc82eb985696377
Andrei Dima <andrei.dima@usm.ro>
2024-01-02 16:18:27 +0200

    a

--- resources/views/useri/form.blade.php
+++ resources/views/useri/form.blade.php
@@ -38,14 +38,14 @@
                 </div>
             </div>
             <div class="col-lg-6 mb-4">
-                <label for="text" class="mb-0 ps-3">Email<span class="text-danger">*</span></label>
-                <input
+                <label for="email" class="mb-0 ps-3">Email<span class="text-danger">*</span></label>
+                {{-- <input
                     type="text"
                     class="form-control bg-white rounded-3 {{ $errors->has('email') ? 'is-invalid' : '' }}"
-                    name="text"
+                    name="email"
                     placeholder=""
-                    {{-- value="{{ old('email', $user->email) }}" --}}
-                    autocomplete="off">
+                    value="{{ old('email', $user->email) }}"
+                    autocomplete="off"> --}}
             </div>
             <div class="col-lg-6 mb-4">
                 <label for="telefon" class="mb-0 ps-3">Telefon</label>
