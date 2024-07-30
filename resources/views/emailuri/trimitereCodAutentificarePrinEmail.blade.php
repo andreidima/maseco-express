430b0a52a10cb69df0c8ac169a74491dbb9ec37b
Andrei <andrei.dima@usm.ro>
2024-07-30 17:39:19 +0300

    a

--- resources/views/emailuri/trimitereCodAutentificarePrinEmail.blade.php
+++ resources/views/emailuri/trimitereCodAutentificarePrinEmail.blade.php
@@ -1,3 +1,7 @@
+@php
+    use Carbon\Carbon;
+@endphp
+
 <div style="margin:0 auto;width:100%; background-color:#eff1f0;">
     <div style="margin:0 auto; max-width:800px!important; background-color: white;">
 
@@ -7,6 +11,8 @@
             Bună {{ $user->name }},
             <br><br>
             Codul generat pentru logare în aplicație este <span style="font-weight: bold; font-size:200%">{{ $user->cod_email }}</span>
+            <br>
+            Următorul cod poate fi generat la {{ Carbon::parse($user->updated_at)->addMinutes(5)->isoFormat('HH:mm DD.MM.YYYY') }}
             <br><br>
             Acesta este un mesaj automat. Vă rugăm să nu răspundeți la acest e-mail.
             <br><br>
