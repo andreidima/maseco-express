4d01c7b4e17cc3bf24e264adb8e3776a11798657
Andrei <andrei.dima@usm.ro>
2024-11-15 10:43:59 +0100

    a

--- resources/views/emailuri/trimitereCodAutentificarePrinEmail.blade.php
+++ resources/views/emailuri/trimitereCodAutentificarePrinEmail.blade.php
@@ -2,8 +2,8 @@
     use Carbon\Carbon;
 @endphp
 
-<div style="margin:0 auto;width:100%; background-color:#eff1f0;">
-    <div style="margin:0 auto; max-width:800px!important; background-color: white;">
+{{-- <div style="margin:0 auto;width:100%; background-color:#eff1f0;">
+    <div style="margin:0 auto; max-width:800px!important; background-color: white;"> --}}
 
         {{-- @include ('emailuri.headerFooter.header') --}}
 
@@ -19,8 +19,8 @@
             Mulțumim! --}}
             (Mesaj automat – te rugăm să nu răspunzi.)
         </div>
-    </div>
+    {{-- </div> --}}
 
     {{-- @include ('emailuri.headerFooter.footer') --}}
-</div>
+{{-- </div> --}}
 
