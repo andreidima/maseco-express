1ffb3ce0a48bc83191739eccf153cc3618c7f319
Andrei <andrei.dima@usm.ro>
2025-02-18 15:07:13 +0100

    Add commands extern observations to email that is send to the transporter when the contract is sent

--- resources/views/emailuri/comenzi/trimiteContractComandaCatreTransportator.blade.php
+++ resources/views/emailuri/comenzi/trimiteContractComandaCatreTransportator.blade.php
@@ -8,9 +8,10 @@
             <br><br>
             Vă trimitem atașat comanda {{ $comanda->transportator_contract }}
 
+            <br><br>
+
             {{-- format documente = 2 means digital --}}
             @if ($comanda->transportator_format_documente == "2")
-                <br><br>
                 Vă rugăm ca după livrarea acestui transport, să încărcați toate documentele aferente în aplicația noastră, în format digital PDF, la adresa
                 <a href="{{ url('/comanda-incarcare-documente-de-catre-transportator/' . $comanda->cheie_unica) }}">
                     {{ url('/comanda-incarcare-documente-de-catre-transportator/' . $comanda->cheie_unica) }}</a>
@@ -19,9 +20,11 @@
                     Dacă documentele nu sunt încărcate în 24 ore de la finalizarea transportului, termenul de plata se prelungește cu 30 de zile!
                 </span>
             @else
-                <br><br>
-                <h3 style="margin: 0px">Documentele aferente acestui transport trebuie trimise în original prin curier, în maxim 15 zile de la finalizarea descărcării, la adresa menționată în comanda de transport!</h3>
+                <h3 style="margin: 0px;">Documentele aferente acestui transport trebuie trimise în original prin curier, în maxim 15 zile de la finalizarea descărcării, la adresa menționată în comanda de transport!</h3>
             @endif
+
+            <br><br>
+            Observații: <span style="white-space: pre-wrap;">{{ $comanda->observatii_externe }}</span>
 
             {{-- <br><br>
             Acesta este un mesaj automat. Vă rugăm să nu răspundeți la acest e-mail. --}}
