bae9e6994127109c79cba8a17ca06be165cc60b8
Andrei <andrei.dima@usm.ro>
2025-08-20 09:13:30 +0300

    Login problems

--- routes/web.php
+++ routes/web.php
@@ -98,7 +98,30 @@ Route::get('/debug-login-sql', function () {
         'raw_password'        => $rawPass ? substr($rawPass, 0, 10) : null,
     ], 200, [], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 });
+Route::get('/debug-charset', function () {
+    $vars = DB::selectOne("
+        SELECT
+          @@character_set_client   AS c_client,
+          @@character_set_connection AS c_conn,
+          @@character_set_results  AS c_results,
+          @@character_set_server   AS c_server,
+          @@collation_connection   AS coll_conn,
+          @@collation_server       AS coll_server,
+          DATABASE()               AS db
+    ");
+    $row = DB::table('users')->where('id',1)->first();
 
+    return response()->json([
+        'db' => $vars->db ?? null,
+        'character_sets' => $vars,
+        'first_row_preview' => [
+            'id' => $row->id ?? null,
+            'name' => isset($row->name) ? substr((string)$row->name, 0, 40) : null,
+            'email' => $row->email ?? null,
+            'password_prefix' => isset($row->password) ? substr((string)$row->password, 0, 10) : null,
+        ],
+    ], 200, [], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+});
 
 
 
