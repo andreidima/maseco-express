804c8312be76aff9f56a8a755f03b4358d99b602
Andrei <andrei.dima@usm.ro>
2025-08-20 09:53:39 +0300

    Login problems

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -56,14 +56,10 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
      */
     protected function gate(): void
     {
-        // Gate::define('viewTelescope', function ($user) {
-        //     return in_array($user->email, [
-        //         //
-        //     ]);
-        // });
-//
-        Gate::define('viewTelescope', function ($user = null) {
-            return true; // allow everyone temporarily
+        Gate::define('viewTelescope', function ($user) {
+            return in_array($user->email, [
+                //
+            ]);
         });
     }
 }
