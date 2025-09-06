2ca50aa4d9fa41cd50c906aaf75c739a511adf4e
Andrei <andrei.dima@usm.ro>
2025-09-06 22:30:52 +0200

    Added schedule command, which is executed in a cronjob, to keep the oferte_curse table slim

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -4,7 +4,6 @@ namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 use Illuminate\Console\Scheduling\Schedule;
-use App\Models\OfertaCursa;
 
 class Kernel extends HttpKernel
 {
