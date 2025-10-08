430345e5b3b3f7498147e85521dabbd8ec43d9ce
Andrei Dima <51689479+andreidima@users.noreply.github.com>
2025-10-08 09:09:58 +0200

    Fix FacturaFurnizor factory namespace

--- database/factories/FacturiFurnizori/FacturaFurnizorFactory.php
+++ database/factories/FacturiFurnizori/FacturaFurnizorFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Database\Factories;
+namespace Database\Factories\FacturiFurnizori;
 
 use App\Models\FacturiFurnizori\FacturaFurnizor;
 use Illuminate\Database\Eloquent\Factories\Factory;
