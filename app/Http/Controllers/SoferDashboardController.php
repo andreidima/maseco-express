b8b850ab433cfa8f83e9d7d96cb85dfd710d97f6
Andrei Dima <51689479+andreidima@users.noreply.github.com>
2025-11-13 12:25:12 +0100

    Remove legacy MasinaValabilitati data from sofer dashboard

--- app/Http/Controllers/SoferDashboardController.php
+++ app/Http/Controllers/SoferDashboardController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\MasinaValabilitati;
 use App\Models\Valabilitate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
@@ -31,54 +30,8 @@ class SoferDashboardController extends Controller
                 ->first();
         }
 
-        $legacyValabilitati = MasinaValabilitati::query()
-            ->select([
-                'id',
-                'nr_auto',
-                'nume_sofer',
-                'detalii_sofer',
-                'divizie',
-                'valabilitate_1',
-                'valabilitate_1_inceput',
-                'valabilitate_1_sfarsit',
-                'observatii_1',
-                'valabilitate_2',
-                'valabilitate_2_inceput',
-                'valabilitate_2_sfarsit',
-                'observatii_2',
-            ])
-            ->where(function ($query) use ($today) {
-                $query->where(function ($subQuery) use ($today) {
-                    $subQuery
-                        ->whereNotNull('valabilitate_1_sfarsit')
-                        ->whereDate('valabilitate_1_sfarsit', '>=', $today);
-                })
-                ->orWhere(function ($subQuery) use ($today) {
-                    $subQuery
-                        ->whereNull('valabilitate_1_sfarsit')
-                        ->whereNotNull('valabilitate_1_inceput')
-                        ->whereDate('valabilitate_1_inceput', '<=', $today);
-                })
-                ->orWhere(function ($subQuery) use ($today) {
-                    $subQuery
-                        ->whereNotNull('valabilitate_2_sfarsit')
-                        ->whereDate('valabilitate_2_sfarsit', '>=', $today);
-                })
-                ->orWhere(function ($subQuery) use ($today) {
-                    $subQuery
-                        ->whereNull('valabilitate_2_sfarsit')
-                        ->whereNotNull('valabilitate_2_inceput')
-                        ->whereDate('valabilitate_2_inceput', '<=', $today);
-                });
-            })
-            ->orderByRaw('COALESCE(valabilitate_1_sfarsit, valabilitate_2_sfarsit) IS NULL ASC')
-            ->orderByRaw('COALESCE(valabilitate_1_sfarsit, valabilitate_2_sfarsit) ASC')
-            ->orderBy('nr_auto')
-            ->get();
-
         return view('sofer.dashboard', [
             'activeValabilitate' => $activeValabilitate,
-            'legacyValabilitati' => $legacyValabilitati,
         ]);
     }
 }
