535598dfd5d85b1ee0222bddcb3868f8edb23c20
Andrei <andrei.dima@usm.ro>
2026-01-16 21:38:09 +0100

    Extended KPI to tracking users

--- resources/views/acasa.blade.php
+++ resources/views/acasa.blade.php
@@ -248,7 +248,7 @@
         </div>
         <div class="col-md-5">
             <div class="row">
-                <div class="col-md-12 mb-5">
+                <div class="col-md-12 mb-2">
                     <div class="table-responsive rounded">
                         <table class="table table-sm table-striped table-hover rounded">
                             <thead class="text-white rounded culoare2">
