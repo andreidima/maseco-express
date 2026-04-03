2df1799ff0a2dcf90c11ed1f145bd9603f19d6f4
Andrei <andrei.dima@usm.ro>
2026-04-03 08:39:37 +0200

    Added the posibility for transporters to mark the invoice files

--- app/Http/Controllers/FacturaTransportatorController.php
+++ app/Http/Controllers/FacturaTransportatorController.php
@@ -97,9 +97,9 @@ class FacturaTransportatorController extends Controller
 
         $comenzi = $query
             ->orderByRaw('data_scadenta_plata_transportator IS NULL')
-            ->orderBy('data_scadenta_plata_transportator')
+            ->orderByDesc('data_scadenta_plata_transportator')
             ->orderByRaw('data_factura_transportator IS NULL')
-            ->orderBy('data_factura_transportator')
+            ->orderByDesc('data_factura_transportator')
             ->orderByDesc('id')
             ->simplePaginate(25)
             ->withQueryString();
